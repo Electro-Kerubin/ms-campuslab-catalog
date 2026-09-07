7c1ae2e2248e60d49807d8b29d273405a8a1b781
VA- Alumno <VA-Alumno@duoc.acad>
2026-09-07 20:31:13 -0300

    modelo datos

---
+++ src/main/resources/db/migration/V2__seed_resource_categories.sql
@@ -0,0 +1,5 @@
+-- Datos de referencia iniciales para el catálogo de categorías.
+INSERT INTO resource_categories (name, description) VALUES
+    ('Equipos de laboratorio', 'Instrumental y equipos reutilizables asignados a un laboratorio'),
+    ('Insumos consumibles',    'Materiales de uso único o recargable (reactivos, guantes, etc.)'),
+    ('Salas',                  'Espacios físicos reservables dentro de un laboratorio');
